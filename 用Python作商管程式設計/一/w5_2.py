370574557e5d82c595b3a99e3c7181564cd32662
Henry Wang <henry940129@gmail.com>
2021-08-11 04:43:50 +0800

    modified

--- 用Python作商管程式設計/一/w5_2.py
+++ 用Python作商管程式設計/一/w5_2.py
@@ -14,3 +14,30 @@ for i in range(n):
     y.append(yi)
     people.append(people_i)
 
+best_town = []
+people_covered_total = 0
+for i in range(p):
+    best_town_current = town[0]
+    people_covered_most = 0
+    best_town_covered = []
+    for j in town:
+        people_covered = people[j]
+        town_covered = [j]
+        for k in town:
+            if k == j:
+                continue
+            if distance(x[j], y[j], x[k], y[k]) <= d:
+                people_covered += people[k]
+                town_covered += [k]
+        if people_covered > people_covered_most:
+            best_town_covered = town_covered
+            people_covered_most = people_covered
+            best_town_current = j
+    best_town.append(best_town_current + 1)
+    people_covered_total += people_covered_most
+    for j in best_town_covered:
+        town.remove(j)
+
+for i in best_town:
+    print(i, end=" ")
+print(people_covered_total, end="")
