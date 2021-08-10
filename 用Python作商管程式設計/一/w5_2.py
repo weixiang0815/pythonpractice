80df974cfde80c20abfb0cebdc0fddf3f561ccb1
Henry Wang <henry940129@gmail.com>
2021-08-11 04:19:06 +0800

    modified

--- 用Python作商管程式設計/一/w5_2.py
+++ 用Python作商管程式設計/一/w5_2.py
@@ -6,6 +6,11 @@ def distance(x1, y1, x2, y2):
 
 
 n, p, d = map(int, input().split())  # 城鎮總數，預計選擇城鎮個數，基地台有效距離
-x, y, people = [0] * n, [0] * n, [0] * n  # x 座標， y 座標，城鎮人口數
+town = [i for i in range(n)]
+x, y, people = [], [], []  # x 座標，y 座標，城鎮人口數
 for i in range(n):
-    x[i], y[i], people[i] = map(int, input().split())
+    xi, yi, people_i = map(int, input().split())
+    x.append(xi)
+    y.append(yi)
+    people.append(people_i)
+
